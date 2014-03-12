816d81cbfd2a947a47cbd01e84cf82cefb019f41
Dolph Mathews <dolph.mathews@gmail.com>
2014-03-12 12:06:01 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='pasteraw',
-    version='1.0.1',
+    version='1.0.2',
     description='Pipe stdin to a raw pastebin',
     author='Dolph Mathews',
     author_email='dolph.mathews@gmail.com',
